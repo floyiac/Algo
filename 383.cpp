5ae32f7098a221b5fd3efb042677036ce3f235a1
floyia <1554484350@qq.com>
2023-09-29 23:47:02 -0400

    Create 383.cpp

---
+++ 383.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int a[26] = {0}; //ransom 
+        int b[26] = {0}; //magazine
+        for(int j=0; j<magazine.size();j++){
+            b[magazine[j]-'a']++;
+        }
+        for(int i=0; i<ransomNote.size();i++){
+            a[ransomNote[i]-'a']++;
+        }
+        for(int k=0;k<26;k++){
+            if(a[k] > b[k]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
