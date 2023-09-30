b5c3739f4127209e6a07c1159df34a2b2f3a0435
floyia <1554484350@qq.com>
2023-09-29 21:35:59 -0400

    Update 1

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // value as key, i as value;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> solution;
+        for (int i=0; i < nums.size(); i++){
+            auto it = solution.find(target - nums[i]);
+            if(it != solution.end()){
+                // finds the element
+                return {it->second, i};
+            }
+            else{
+                solution.insert(pair<int,int>(nums[i],i));
+            }
+        }
+        return {};
+    }
+};
