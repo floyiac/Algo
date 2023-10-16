1e92d499794c4535b45a7540d012a66088aabce0
floyia <1554484350@qq.com>
2023-10-15 22:48:57 -0400

    Create 222.cpp

---
+++ binary_tree/222.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root){
+        if(root == NULL){return 0;}
+        int A = helper(root->left);
+        int B = helper(root->right);
+        return A+B+1;
+    }
+    int countNodes(TreeNode* root) {
+        return helper(root);
+    }
+};
