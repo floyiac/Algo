af7e20e240b1a3515bd3fd020cd4d3eb43fb001a
floyia <1554484350@qq.com>
2023-11-14 00:01:06 -0500

    Create 225.cpp

---
+++ stack_queue/225.cpp
@@ -0,0 +1,42 @@
+// One queue is enough. Put the elements of queue to the back except the last one.
+class MyStack {
+public:
+    queue<int> que;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        que.push(x);
+    }
+    
+    int pop() {
+        int size = que.size();
+        while (size >1){
+            int temp = que.front();
+            que.pop();
+            que.push(temp);
+            size --;
+        }
+        int res = que.front();
+        que.pop();
+        return res;
+    }
+    
+    int top() {
+        return que.back();
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
