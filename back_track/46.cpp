68bea07bed722fdb77dafc557e831a7d09ed53a4
floyia <1554484350@qq.com>
2024-01-09 15:23:00 -0500

    Create 46.cpp

---
+++ back_track/46.cpp
@@ -0,0 +1,27 @@
+vector<int> path;
+vector<vector<int>> res;
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        path.clear();
+        res.clear();
+        vector<bool> used(nums.size(),false);
+        backtrack(nums,used);
+        return res;
+    }
+
+    void backtrack(vector<int>&nums, vector<bool>& used){
+        if (path.size() == nums.size()){
+            res.push_back(path);
+            return;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(used[i]){continue;}
+            used[i] = true;
+            path.push_back(nums[i]);
+            backtrack(nums,used);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+};
