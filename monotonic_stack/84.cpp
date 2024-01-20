a23e7a4087e0aa669410ca753c0d7df293d31586
Zhuowei Chen <1554484350@qq.com>
2024-01-19 20:30:20 -0500

    Update 84.cpp

--- monotonic_stack/84.cpp
+++ monotonic_stack/84.cpp
@@ -4,13 +4,13 @@ public:
         stack<int> st;
         heights.insert(heights.begin(),0);
         heights.push_back(0);
-        st.push(0);
         int res = 0;
-        for(int i=1; i < heights.size(); i++){
-            while(heights[i] < heights[st.top()]){
+        st.push(0);
+        for(int i=1;i<heights.size();i++){
+            while(heights[i]<heights[st.top()]){
                 int mid = st.top();
                 st.pop();
-                int w = i - st.top() - 1;
+                int w = i - st.top() - 1; // the middle area
                 int h = heights[mid];
                 res = max(res,w*h);
             }
