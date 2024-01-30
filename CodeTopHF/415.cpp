51c2abc1d96132ed0b2369423fd9487e6bb699e6
Zhuowei Chen <1554484350@qq.com>
2024-01-30 09:02:12 -0500

    Create 415.cpp

---
+++ CodeTopHF/415.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string res;
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        int carry = 0;
+        while(i>=0 || j>=0 || carry){
+            int sum = 0;
+            if(i>=0){
+                sum += num1[i] - '0';
+                i--;
+            }
+            if(j>=0){
+                sum += num2[j] - '0';
+                j--;
+            }
+            sum += carry;
+            carry = sum/10;
+            res.push_back(sum%10 + '0');
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
