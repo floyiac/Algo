ee381fcd77ae925f3a575653e48c670ca75c3243
floyia <1554484350@qq.com>
2024-02-06 10:27:53 -0500

    Create 148.cpp

---
+++ CodeTopHF/148.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* prev = nullptr;
+        while(fast && fast->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = nullptr;
+
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(slow);
+
+        return merge(l1,l2);
+    }
+
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                tail->next = l1;
+                l1 = l1->next;
+                tail = tail->next;
+            }
+            else{
+                tail->next = l2;
+                l2 = l2->next;
+                tail = tail->next;
+            }
+        }
+        while(l1){
+            tail->next = l1;
+            l1 = l1->next;
+            tail = tail->next;
+        }
+        while(l2){
+            tail->next = l2;
+            l2 = l2->next;
+            tail = tail->next;
+        }
+        return dummy.next;
+    }
+};
