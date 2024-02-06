b0e4faa32aa543dd366ee5bdb9fcc5fa0bd596ae
floyia <1554484350@qq.com>
2024-02-06 09:02:43 -0500

    Create 31.cpp

---
+++ CodeTopHF/31.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-2;
+        int j = n-1;
+        for(;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                break;
+            }
+        }
+        if(i>=0){
+            for(;j>=0;j--){
+                if(nums[j] > nums[i]){
+                    break;
+                }
+            }
+            swap(nums[i],nums[j]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
