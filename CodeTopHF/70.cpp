10e8f0e7ddcb6e1993aeaf7be33c0b27e30151b4
floyia <1554484350@qq.com>
2024-02-08 22:20:40 -0500

    Create 70.cpp

---
+++ CodeTopHF/70.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int dp[3];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2;i<n;i++){
+            dp[2] = dp[1] + dp[0];
+            dp[0] =dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
+    }
+};
