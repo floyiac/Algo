c4403989ee40573cfef641183c52dd157de23bf4
floyia <1554484350@qq.com>
2024-02-19 09:30:07 -0500

    Update 151.cpp

--- CodeTopHF/151.cpp
+++ CodeTopHF/151.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     void trimSpace(string& s){
         int start = 0;
-        int end = s.length() - 1;
+        int end = s.length() -1;
         while(start <= end && s[start] == ' ') start ++;
         while(end >= start && s[end] == ' ') end--;
-        s = s.substr(start,end-start+1);
+        s = s.substr(start, end-start+1);
     }
     string reverseWords(string s) {
         trimSpace(s);
@@ -13,20 +13,22 @@ public:
         int start = 0;
         for(int i=0;i<=s.length();i++){
             if(i == s.length() || s[i] == ' '){
-                reverse(s.begin()+start,s.begin() + i);
-                start = i+1;
+                reverse(s.begin() + start, s.begin() + i);
+                start =  i + 1;
             }
         }
         string res;
         bool inWord = false;
         for(char c : s){
-            if(c != ' '){
-                if(!inWord && !res.empty()) res += ' ';
-                inWord = true;
-                res += c;
+            if(c == ' '){
+                inWord = false;
             }
             else{
-                inWord = false;
+                if(!inWord && !res.empty()){
+                    res += ' ';
+                }
+                inWord = true;
+                res += c;
             }
         }
         return res;
