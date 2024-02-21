ec0d1aa4d17eabf352147ffdd8b31206aaadbcbe
floyia <1554484350@qq.com>
2024-02-21 11:27:20 -0500

    Update 110.cpp

--- CodeTopHF/110.cpp
+++ CodeTopHF/110.cpp
@@ -12,15 +12,15 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return DFS(root) != -1;
+        return dfsSearch(root) != -1;
     }
-    int DFS(TreeNode* node){
+    int dfsSearch(TreeNode* node){
         if(node == nullptr) return 0;
-        int leftheight = DFS(node->left);
-        if(leftheight == -1) return -1;
-        int rightheight = DFS(node->right);
-        if(rightheight == -1) return -1;
-        if(abs(leftheight-rightheight) > 1) return -1;
-        return max(leftheight,rightheight) + 1;
+        int left = dfsSearch(node->left);
+        if(left == -1) return -1;
+        int right = dfsSearch(node->right);
+        if(right == -1) return -1;
+        if(abs(right-left) > 1) return -1;
+        return max(right,left) + 1;
     }
 };
